d01adbb59a945034cf50a3c244237d1aecebda8c
bharathkallurs <bharathkallurs@gmail.com>
2012-12-30 08:32:35 -0800

    Create Life_universe_everything.py

---
+++ Life_universe_everything.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python -tt
+# http://www.spoj.pl/problems/TEST/
+
+import sys
+import re
+
+def main() :
+  while 1 : 
+		num = int(input())
+		if num == 42 : break
+		else : print(num)
+		
+	
+	
+if __name__ == '__main__' :
+	main()
